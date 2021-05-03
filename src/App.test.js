550f31207c1bd24448c6c578a945bbaade902263
Yago Gonzalez <jagogc@gmail.com>
2021-05-03 22:19:52 +0100

    Remove enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,8 @@
 import React from "react";
-import { shallow } from "enzyme";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
-
 describe("App", () => {
-  it("enzyme - renders without crashing", () => {
-    const element = shallow(<App />);
-    expect(element).toBeDefined();
-  });
-
   it("@testing-library/react -renders without crashing", () => {
     const container = render(<App />);
     expect(container).toBeDefined();
